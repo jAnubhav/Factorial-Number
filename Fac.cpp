5f8081af1d8becd0df4004f935bf5acad3c0da1b
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2021-07-22 18:24:30 +0530

    Update Fac.cpp

--- Fac.cpp
+++ Fac.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    int fac = 1;
+    for (int i = 1; i != num+1; i++) {
+        fac *= i;
+    }
+    return fac;
+}
 
 int main() {
-    
+    int num;
+    cout<<"Enter a Number";
+    cin>>num;
+
+    int fac = factorial(num);
+    cout<<"The factorial of the number is " + fac;
     return 0;
 }
