c88d8c7d6db30c00376c79e666591d2511d863a2
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2021-07-22 17:56:15 +0530

    Create fac.py

---
+++ fac.py
@@ -0,0 +1,10 @@
+def factorial(num):
+  fac = 1
+  for i in range(1, num+1):
+    fac *= i
+  return fac
+
+if __name__ == "__main__":
+  num = int(input("Enter a number : "))
+  fac = factorial(num)
+  print("Factorial of the number is", fac)
