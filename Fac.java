ce0852740c6ebc4f1dca34c495276850fe149dd9
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2021-07-22 18:07:29 +0530

    Update Fac.java

--- Fac.java
+++ Fac.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Fac {
+    public static int factorial(int num) {
+        int fac = 1;
+        for (int i = 1; i != num+1; i++) {
+            fac *= i;
+        }
+        return fac;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int num = sc.nextInt();
+        sc.close();
+        
+        int fac = factorial(num);
+        System.out.println("The Factorial of the number is " + fac);
+    }
+}
